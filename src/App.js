46c287fe51c49c8c4a85b94258aabc19f358bd0b
Rebecca Williams <rebecca@informedk12.com>
2022-09-17 17:31:52 -0700

    Conditionally render the button content.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import './App.css';
 import { PauseIcon, ResetIcon, StartIcon } from './icons';
-
+import React, { useState } from 'react';
 
 function App() {
+
+  const [isOn, setIsOn] = useState(false);
+
+  const renderStartContent = () => {
+
+    return (
+      <React.Fragment>
+        <StartIcon />
+        Start
+      </React.Fragment>
+    );
+  }
+
+  const renderPauseContent = () => {
+
+    return (
+      <React.Fragment>
+        <PauseIcon />
+        Pause
+      </React.Fragment>
+    );
+  }
+
   return (
     <div className="container">
       <div className='time'>
         00:00
       </div>
       <div className='buttons'>
-      <button className='button button--active'>
-          <StartIcon />
-          Start
-        </button>
-        <button className='button button--inactive'>
-          <PauseIcon />
-          Pause
+        <button
+          className={`button ${isOn ? 'button--active' : 'button--inactive'}`}
+          onClick={() => setIsOn(() => !isOn)}
+        >
+          {isOn ? renderStartContent() : renderPauseContent()}
         </button>
         <button className='button button--secondary'>
           <ResetIcon />
