f10fb56c2ecf737129bf244dedb521fc462941f1
Rebecca Williams <rebecca@informedk12.com>
2022-10-02 23:46:00 -0500

    Clean up code

--- src/App.js
+++ src/App.js
@@ -4,77 +4,60 @@ import React, { useEffect, useState } from 'react';
 
 function App() {
 
-  const [isOn, setIsOn] = useState(false);
-
+  const [isActive, setIsActive] = useState(false);
   const [seconds, setSeconds] = useState(0);
 
-  const onReset = () => {
-    setIsOn(false);
-    setSeconds(0);
-  }
-
-  const formatTime = () => {
-    let hours = Math.floor(seconds / 3600); // returns the number of full hours
-    let minutes = Math.floor((seconds % 3600) / 60); // returns the number of full minutes that remain after getting the number of full hours
-    let remainingSeconds = Math.floor(seconds % 60); // returns the remaining seconds
-    let formattedTime = '';
-
-    if (hours > 0) {
-      formattedTime = `${hours}:${minutes < 10 ? '0' : ''}`
-    }
-
-    formattedTime = `${formattedTime}${minutes}:${(remainingSeconds < 10 ? '0' : '')}`;
-    return `${formattedTime}${remainingSeconds}`;
-  }
-
   useEffect(() => {
-    if (isOn) {
+    if (isActive) {
       const interval = setInterval(() => {
-        setSeconds(seconds => seconds + 1);
-      }, 1000);
-      return () => clearInterval(interval);
+        setSeconds(prevSeconds => prevSeconds + 1)
+      }, 1000)
+      return () => clearInterval(interval)
     }
-  }, [isOn, seconds]);
+  }, [isActive, seconds])
+
+  const toggle = () => {
+    setIsActive(prevState => !prevState)
+  }
+
+  const reset = () => {
+    setIsActive(false);
+    setSeconds(0)
+  }
+
+  const buttonContent = () => {
+    if(isActive) {
+      return (
+        <React.Fragment>
+          <PauseIcon />
+          Pause
+        </React.Fragment>
+      );
+    }
+    return (
+      <React.Fragment>
+        <StartIcon />
+        Start
+      </React.Fragment>
+    )
+  }
 
   return (
     <main className="container">
       <h1 className='heading'>Stopwatch</h1>
       <div className='time'>
-        {formatTime(seconds)}
+        {seconds} <abbr title='seconds'>s</abbr>
       </div>
       <div className='buttons'>
-        <button
-          className={`button ${isOn ? 'button--inactive' : 'button--active'}`}
-          onClick={() => setIsOn(() => !isOn)}
-        >
-          {isOn ? renderPauseContent() : renderStartContent()}
+        <button className={`button ${isActive ? 'button--inactive' : 'button--active'}`} onClick={toggle}>
+          {buttonContent()}
         </button>
-        <button className='button button--secondary' onClick={onReset}>
+        <button className='button button--secondary' onClick={reset}>
           <ResetIcon />
           Reset
         </button>
       </div>
     </main>
-  );
-}
-
-const renderStartContent = () => {
-
-  return (
-    <React.Fragment>
-      <StartIcon />
-      Start
-    </React.Fragment>
-  );
-}
-
-const renderPauseContent = () => {
-
-  return (
-    <React.Fragment>
-      <PauseIcon />
-      Pause
-    </React.Fragment>
   );
 }
 
